1b0bfc4bb28ad209dacca31ac237c8f9812b9632
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-02-09 15:31:45 -0800

    TestPage1

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -2,12 +2,29 @@
 
 function Page1() {
     return (
-        <div className="grid grid-cols-3 shadow-xl" >
-            <h1 className="text-red-500 text-2xl font-bold underline border-2 border-red-500 animate-bounce ">
-                Tailwind className Example!
-            </h1>
+        <div>
+            <div>
+                <img src="./Chanelimage.jpg" alt="Image of a perfume" />
+            </div>
 
-            <div class=""></div>
+            {/* <!-- add to cart wrapper --> */}
+            <div>
+                <h5>PERFUME</h5>
+                <h1>Gabrielle Essence Eau De Parfum</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, perspiciatis. Cumque temporibus fuga
+                    incidunt impedit voluptate reprehenderit dolorum </p>
+
+                {/* <!-- price wrapper --> */}
+                <div>
+                    <h2>$149.99</h2>
+                    <p>$169.99</p>
+                </div>
+                <br /><br /><br />
+
+                {/* <!-- Add to cart button wrapper --> */}
+                <button>Add to Cart</button>
+
+            </div>
 
         </div>
     );
