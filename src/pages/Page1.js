cc9a2160254bf2e050760044ea89640deeef77bd
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-02-09 16:05:02 -0800

    ChanelWebReact

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,31 +1,38 @@
-
+import Chanelimage from "../assets/images/Chanelimage.jpg";
 
 function Page1() {
     return (
-        <div>
+
+        <div class="mainContainer">
+
+            {/* <!-- leftContainer --> */}
             <div>
-                <img src="./Chanelimage.jpg" alt="Image of a perfume" />
+                <img src={Chanelimage} alt="Chanelimage" />
             </div>
 
-            {/* <!-- add to cart wrapper --> */}
-            <div>
-                <h5>PERFUME</h5>
-                <h1>Gabrielle Essence Eau De Parfum</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, perspiciatis. Cumque temporibus fuga
-                    incidunt impedit voluptate reprehenderit dolorum </p>
+            {/* <!-- rightContainer --> */}
+            <div class="rightContainer">
+                {/* <h6 style="letter-spacing: 7px;">PERFUME</h6> */}
 
-                {/* <!-- price wrapper --> */}
-                <div>
-                    <h2>$149.99</h2>
+                <h6>PERFUME</h6>
+                <h2>Gabrielle Essence Eau De Parfum</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Corrupti, perspiciatis. Cumque temporibus fuga incidunt
+                    impedit voluptate reprehenderit dolorum</p>
+
+                {/* <!-- pricingContainer --> */}
+
+                <div class="pricingContainer">
+                    {/* <p style="color: green; font-size: 30px;"><b>$149.99</b></p> */}
+
+                    <p><b>$149.99</b></p>
+                    {/* <p style="text-decoration: line-through; margin-left: 25px;">$169.99</p> */}
+
                     <p>$169.99</p>
-                </div>
-                <br /><br /><br />
+                </div><br /><br /><br />
 
-                {/* <!-- Add to cart button wrapper --> */}
-                <button>Add to Cart</button>
-
+                <button type="button" onclick="myFunction()">Add to Cart</button>
             </div>
-
         </div>
     );
 }
