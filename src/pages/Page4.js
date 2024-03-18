9882bf4b6d7b2a819eab3cdd0a3d1df683d8e8e3
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-03-17 20:49:32 -0800

    BeverageStockTracker

--- src/pages/Page4.js
+++ src/pages/Page4.js
@@ -1,9 +1,44 @@
-
+import React from "react";
+import { useState } from "react";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Select } from '@chakra-ui/react'
 
 function Page4() {
     return (
         <div className="bg-yellow-500" >
-            <h1>Page4 CODE GOES HERE</h1>
+            <h1>Akusika Beverages Stock Tracker</h1><br />
+            <form>
+                <label for="products">Select a product: </label>
+
+                <Select placeholder='Select option'
+                    onChange={(e) => {
+                        setProductName(e.target.value)
+                    }} name="Products">
+                    <Option value='Bigoo cocktail'>Bigoo cocktail</Option>
+                    <Option value='Bigoo lemon'>Bigoo lemon</Option>
+                    <Option value='Bigoo cola'>Bigoo cola</Option>
+                    <Option value='Bigoo grape'>Bigoo grape</Option>
+                    <Option value='Bigoo orange'>Bigoo orange</Option>
+                    <Option value='Bigoo apple'>Bigoo apple</Option>
+                    <Option value='Bigoo energy'>Bigoo energy</Option>
+                    <Option value='Rush energy'>Rush energy</Option>
+                    <Option value='BB cocktail'>BB Coacktail</Option>
+                    <Option value='Vitamilk'>Vitamilk</Option>
+                    <Option value='U fresh soy milk'>U fresh soy milk</Option>
+                    <Option value='U fresh pineaple'>U fresh pineaple</Option>
+                    <Option value='Tampico (500 ml)'>Tampico (500 ml)</Option>
+                    <Option value='Tampico (750 ml)'>Tampico (750 ml)</Option>
+                    <Option value='Tampico (1 L)'>Tampico (1 L)</Option>
+                    <Option value='Bel cola'>Bel cola</Option>
+                    <Option value='Bel malt'>Bel malt</Option>
+                    <Option value='Bigoo active'>Bel active</Option>
+                    <Option value='Bigoo aqua (500 ml)'>Bel aqua (500 ml)</Option>
+                    <Option value='Bigoo aqua (750 ml)'>Bel aqua (750 ml)</Option>
+                    <Option value='Malta guinness (can)'>Malta guinness (can)</Option>
+                    <Option value='Malta guinness (plastic)'>Malta guinness (plastic)</Option>
+                    <Option value='Sachet water'>Sachet water</Option>
+                </Select>
+            </form>
         </div>
     );
 }
--- src/pages/Page5.js
+++ src/pages/Page5.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState } from "react";
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { Select } from '@chakra-ui/react'
 
 function Page5() {
     const [productName, setProductName] = useState()
@@ -15,34 +17,34 @@ function Page5() {
             <form>
                 <label for="products">Select a product: </label>
 
-                <select
+                <Select placeholder='Select option'
                     onChange={(e) => {
                         setProductName(e.target.value)
                     }} name="Products">
-                    <option>Bigoo cocktail</option>
-                    <option>Bigoo lemon</option>
-                    <option>Bigoo cola</option>
-                    <option>Bigoo grape</option>
-                    <option>Bigoo orange</option>
-                    <option>Bigoo apple</option>
-                    <option>Bigoo energy</option>
-                    <option>Rush energy</option>
-                    <option>BB Coacktail</option>
-                    <option>Vitamilk</option>
-                    <option>U fresh soy milk</option>
-                    <option>U fresh pineaple</option>
-                    <option>Tampico (500 ml)</option>
-                    <option>Tampico (750 ml)</option>
-                    <option>Tampico (1 L)</option>
-                    <option>Bel cola</option>
-                    <option>Bel malt</option>
-                    <option>Bel active</option>
-                    <option>Bel aqua (500 ml)</option>
-                    <option>Bel aqua (750 ml)</option>
-                    <option>Malta guinness (can)</option>
-                    <option>Malta guinness (plastic)</option>
-                    <option>Sachet water</option>
-                </select>
+                    <Option value='Bigoo cocktail'>Bigoo cocktail</Option>
+                    <Option value='Bigoo lemon'>Bigoo lemon</Option>
+                    <Option value='Bigoo cola'>Bigoo cola</Option>
+                    <Option value='Bigoo grape'>Bigoo grape</Option>
+                    <Option value='Bigoo orange'>Bigoo orange</Option>
+                    <Option value='Bigoo apple'>Bigoo apple</Option>
+                    <Option value='Bigoo energy'>Bigoo energy</Option>
+                    <Option value='Rush energy'>Rush energy</Option>
+                    <Option value='BB cocktail'>BB Coacktail</Option>
+                    <Option value='Vitamilk'>Vitamilk</Option>
+                    <Option value='U fresh soy milk'>U fresh soy milk</Option>
+                    <Option>U fresh pineaple</Option>
+                    <Option>Tampico (500 ml)</Option>
+                    <Option>Tampico (750 ml)</Option>
+                    <Option>Tampico (1 L)</Option>
+                    <Option>Bel cola</Option>
+                    <Option>Bel malt</Option>
+                    <Option>Bel active</Option>
+                    <Option>Bel aqua (500 ml)</Option>
+                    <Option>Bel aqua (750 ml)</Option>
+                    <Option>Malta guinness (can)</Option>
+                    <Option>Malta guinness (plastic)</Option>
+                    <Option>Sachet water</Option>
+                </Select>
                 <br /><br />
                 <label for="cost price">Input the cost price: </label>
                 <input
