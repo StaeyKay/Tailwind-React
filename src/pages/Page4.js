7f1dd25729b2a7cae543bc876da074128011d28b
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-04-02 12:30:51 -0800

    ReactInlineStyling

--- src/pages/Page4.js
+++ src/pages/Page4.js
@@ -2,25 +2,21 @@ import React from "react";
 import { useState } from "react";
 
 function Page4() {
-    const styles = {
-        innerDiv : {
+    
+    return (
+
+        <div style={{
+            padding: "70px 0",
+            backgroundColor: "orange"
+        }}>
+            <div style={{
             backgroundColor: "#73AD21",
             padding: "70px 0",
             textAlign: "center",
             border: "3px solid black",
             width: "60%",
             margin: "auto"
-        },
-        outerDiv: {
-            padding: "70px 0",
-            backgroundColor: "blue"
-        }
-    }
-    
-    return (
-
-        <div style={styles.outerDiv}>
-            <div style={styles.innerDiv}>This is a green box</div>
+        }}>This is a green box</div>
         </div>
 
     );
