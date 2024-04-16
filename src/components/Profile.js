a26cc29d1a9661085179a4182d353bf4f8c6a27b
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-04-16 15:11:23 -0800

    HandleForm

---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+function Profile () {
+    
+    return (
+        <div>
+            <h1 style={{fontSize: "30px", fontWeight: "500"}}>Profile component</h1>
+        </div>
+    )
+}
+
+export default Profile;
--- src/pages/Page4.js
+++ src/pages/Page4.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { useState } from "react";
+import Profile from "../components/Profile.js";
 
 function Page4() {
 
+    // define a state to show/hide the text
+    const [show, setShow]=useState(false)
     const [name, setName]=useState();
     const [interest, setInterest]=useState();
     const [TnC, setTnC]=useState();
 
     function handleData (e) {
         e.preventDefault()
-        console.log("all data", name, interest, TnC)
     }
+    console.log("all data", name, interest, TnC)
 
     return (
 
         <div>
+
+            {/* state the conditions for hiding/showing the text */}
+            {
+            show?<h1>Hello World!</h1>:null
+            }
+            {/* <button style={{backgroundColor: "green", padding: "10px"}} onClick={()=>setShow(true)}>Show</button>
+            <button style={{backgroundColor: "red", padding: "10px"}} onClick={()=>setShow(false)}>Hide</button> */}
+            <button style={{backgroundColor: "red", padding: "10px"}} onClick={()=>setShow(!show)}>Toggle</button>
+
             <h1>Hello World!</h1>
             <form>
                 <input type="text" placeholder="Enter name" onChange={(e)=>setName(e.target.value)}/><br/><br/>
-                <select onChange={(e)=>setInterest(e.target.value)}>
+                <select onChange={(e)=>{
+                    setInterest(e.target.value)
+                    console.log(e.target.value);
+                    }}>
                     <option>Marvel</option>
                     <option>DC</option>
                 </select><br/><br/>
