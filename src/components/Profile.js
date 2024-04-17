aa77c65fe2364a838b58859ca4c1c5388c481e5e
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-04-16 16:06:20 -0800

    conditionalRenderingComponent

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,18 @@
-function Profile () {
-    
+import React from "react";
+import { useState } from "react";
+
+
+function Profile() {
+    const [loggedIn, setLoggedIn] = useState(2);
+
     return (
         <div>
-            <h1 style={{fontSize: "30px", fontWeight: "500"}}>Profile component</h1>
+            {
+                loggedIn==1?<h1 style={{fontSize: "30px", fontWeight: "500"}}>Welcome User 1</h1>
+                :loggedIn==2?<h1 style={{fontSize: "30px", fontWeight: "500"}}>Welcome User 2</h1>
+                :loggedIn==3?<h1 style={{fontSize: "30px", fontWeight: "500"}}>Welcome User 3</h1>
+                :<h1 style={{fontSize: "30px", fontWeight: "500"}}>Welcome Guest</h1>
+            }
         </div>
     )
 }
